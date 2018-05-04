b0177d3ee0490498510ab2ff11ef257ea675945d
lingjun.jlj <jjl@33.cn>
2018-05-05 00:16:17 +0800

    爬取http://www.mmjpg.com/美女图片

--- spider/test.py
+++ spider/test.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 # @author : lingjun.jlj
 # @create : 2018/5/3
+
 import os
-import re
-
-import bs4
-from lxml import etree
-
 import requests
+from lxml import etree
+import time
+
+from selenium import webdriver
+
+# 将Chrome设置成不加载图片的无界面运行状态
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.managed_default_content_settings.images": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument("--headless")
+chrome_path = '/Users/jianglingjun/Document/PycharmProjects/picture'
 
 # 设置图片存储路径
 PICTURES_PATH = os.path.join(os.getcwd(), 'pictures/')
@@ -33,56 +40,41 @@ class Spider(object):
     def get_page_urls(self):
         if int(page_num) > 1:
             for n in range(2, int(page_num) + 1):
-                page_url = 'http://www.mmjpg.com/' + str(n)
+                page_url = 'http://www.mmjpg.com/home/' + str(n)
                 self.page_urls.append(page_url)
         elif int(page_num) == 1:
             pass
 
-    # 根据页面URL获取进入MM的页面
+    # 获取妹子的url的方法
     def get_girl_urls(self):
         for page_url in self.page_urls:
-            # 获取页面
             html = requests.get(page_url).content
             selector = etree.HTML(html)
-            self.girl_urls += selector.xpath('//span[@class="title"]/a/@href')
+            self.girl_urls += (selector.xpath('//span[@class="title"]/a/@href'))
 
-    # 根据MM页面获取MM详细图片url
+    # 获取图片的url的方法
     def get_pic_urls(self):
-        page_num = 0
+        driver = webdriver.Chrome(chrome_path, chrome_options=chrome_options)
         for girl_url in self.girl_urls:
-            page_num += 1
-            request = requests.get(girl_url, headers=headers)
-            soup = bs4.BeautifulSoup(request.content, 'lxml')
-            img_name = soup.find('img').get('alt')
-            self.girl_name = img_name
-            print(img_name)
-            # 获取有多少页
-            page_total = 8
-            if page_num == 1:
-                page_total = 7
-            img_total = soup.find_all('a', href=re.compile('/mm'))[page_total].get_text().strip()
-            image_urls = []
-            for index in range(1, int(img_total) + 1):
-                pic_url = girl_url + '/' + str(index)
-                request = requests.get(pic_url, headers=headers)
-                soup = bs4.BeautifulSoup(request.content, 'lxml')
-                imge_url = soup.find('img').get('src')
-                image_urls.append(imge_url)
-            # 保存图片地址
-            self.pic_urls = image_urls
+            driver.get(girl_url)
+            time.sleep(3)
+            driver.find_element_by_xpath('//em[@class="ch all"]').click()
+            time.sleep(3)
+            html = driver.page_source
+            selector = etree.HTML(html)
+            self.girl_name = selector.xpath('//div[@class="article"]/h2/text()')[0]
+            self.pic_urls = selector.xpath('//div[@id="content"]/img/@data-img')
             try:
-                self.download_pics()
-                print('-------')
+                self.download_pic()
             except Exception as e:
                 print("{}保存失败".format(self.girl_name) + str(e))
 
-    # 下载MM图片
-    def download_pics(self):
+    # 下载图片的方法
+    def download_pic(self):
         try:
             os.mkdir(PICTURES_PATH)
         except:
             pass
-
         girl_path = PICTURES_PATH + self.girl_name
         try:
             os.mkdir(girl_path)
@@ -99,9 +91,8 @@ class Spider(object):
             else:
                 with open(pic_path, 'wb')as f:
                     f.write(img_data.content)
-                print("正在保存{}第{}张".format(self.girl_name, img_name))
-                f.close()
-
+                    print("正在保存{}第{}张".format(self.girl_name, img_name))
+                    f.close()
         return
 
     # 爬虫的启动方法，按照爬虫逻辑依次调用方法
@@ -111,11 +102,8 @@ class Spider(object):
         self.get_pic_urls()
 
 
-if __name__:
-    page_num = 1
-    MM_Spider = Spider(page_num)
-    MM_Spider.start()
-    page_url = 'http://www.mmjpg.com/home/1'
-    html = requests.get(page_url).content
-    selector = etree.HTML(html)
-    print(selector.xpath('//span[@class="title"]/a/@href'))
+# main函数
+if __name__ == '__main__':
+    page_num = input("请输入页码:")
+    mmjpg_spider = Spider(page_num)
+    mmjpg_spider.start()
